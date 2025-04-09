4c166987af9e0d0bcf83f7c9d7cb7e6edfe4c8ed
Isaias Trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2025-04-09 15:48:18 -0500

    updated data

--- convert-xls-to-json.ts
+++ convert-xls-to-json.ts
@@ -1,6 +1,6 @@
 import { readFile, utils } from "xlsx";
 
-const filename = "horarios-con-rectificaciones";
+const filename = "horarios-de-nivelacion-procesados";
 const path = `data/${filename}.xlsx`;
 
 const file = readFile(path);
