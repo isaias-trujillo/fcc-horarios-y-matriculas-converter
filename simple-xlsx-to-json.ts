6cfcc4a25f2cf76d7206125592519de98fab2491
Isaias Trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2025-04-16 17:07:58 -0500

    updated data

---
+++ simple-xlsx-to-json.ts
@@ -0,0 +1,34 @@
+import {readFile, utils} from 'xlsx'
+
+const filename = 'horarios.miercoles.procesados';
+const path = `data/${filename}.xlsx`;
+
+const file = readFile(path);
+
+if (!file) {
+    throw new Error("File not found");
+}
+
+const sheet = file.SheetNames[0];
+
+if (!sheet) {
+    throw new Error("Sheet not found");
+}
+
+const ws = file.Sheets[sheet];
+
+if (!ws) {
+    throw new Error("Worksheet not found");
+}
+
+const rows = utils.sheet_to_json(ws);
+
+Bun.write(`result/${filename}.json`, JSON.stringify(rows))
+    .then((r) =>
+        console.log(
+            `File result/${filename}.json created with rows: ${rows.length}`,
+        ),
+    )
+    .catch(
+        (e) => `Failed to create 'result/${filename}.json', error: ${e.message}`,
+    );
