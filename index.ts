7415c8bc721efe8e5e42afc27011047a662e3d0a
Isaias Trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2025-04-21 15:05:10 -0500

    added Monday schedules

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import {readFile, utils} from 'xlsx'
 import fixScheduleRow from "./utils/fixScheduleRow.ts";
 import type ScheduleRow from "./types/schedules/ScheduleRow";
 
-const filename = "horarios.domingo.raw";
+const filename = "horarios.lunes.raw";
 const path = `data/${filename}.xlsx`;
 
 const file = readFile(path);
